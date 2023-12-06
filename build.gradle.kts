6af216ef581948481cbbab29711e59c10c774474
alyrow <58048578+alyrow@users.noreply.github.com>
2023-12-06 14:59:37 +0100

    bump deps version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.serialization") version "1.9.21"
 }
 
 group = "fr.corpauration"
